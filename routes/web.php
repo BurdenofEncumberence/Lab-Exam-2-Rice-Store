24370b50b9da9b973aa618af2d6e38c282d94ebd
Jeremy Holden Kent F. Alicando <j.alicando.557052@umindanao.edu.ph>
2026-04-22 13:29:42 +0800

    rice plus index

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('rices');
+    return view('rices.index');
 });
 
 Route::get('/dashboard', function () {
