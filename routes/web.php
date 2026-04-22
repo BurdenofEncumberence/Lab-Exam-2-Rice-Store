120eaa2123bd2053abf8f737f27fc776aaa683d0
Jeremy Holden Kent F. Alicando <j.alicando.557052@umindanao.edu.ph>
2026-04-22 13:37:23 +0800

    update to deploy

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('rices.index');
+    return redirect()->route('rices.index');
 });
 
 Route::get('/dashboard', function () {
